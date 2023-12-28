e057497d9b84f7579e3251866c83084a3a9fb81a
Mohit Soni <sonimohit481@gmail.com>
2023-12-28 19:40:21 +0530

    store.js fixes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,28 @@
-import {applyMiddleware,compose, combineReducers, legacy_createStore as createStore } from "redux"
-import thunk from "redux-thunk"
-import { authReducer } from "./auth/authReducer"
-import  userData from "./userData/userDataReducer"
+import {
+  applyMiddleware,
+  compose,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
+import thunk from "redux-thunk";
+import { authReducer } from "./auth/authReducer";
+import userData from "./userData/userDataReducer";
 
-const rootReducer=combineReducers({
-    auth: authReducer,
-    userData
-})
+const rootReducer = combineReducers({
+  auth: authReducer,
+  userData,
+});
 
-export const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+let middleware = applyMiddleware(thunk);
 
-store.subscriber=()=>{
-    console.log("State:", store.getState());
+if (process.env.NODE_ENV === "development") {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  middleware = composeEnhancers(middleware);
 }
 
+export const store = createStore(rootReducer, middleware);
+
+store.subscriber = () => {
+  console.log("State:", store.getState());
+};
